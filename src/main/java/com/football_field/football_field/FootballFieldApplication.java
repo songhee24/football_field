2917b0d997061b2f37613692f0134ca3f1afc201
Azamat Orozbaev <44497623+songhee24@users.noreply.github.com>
2020-11-11 10:45:21 +0600

    add jwt and some another dependencies. Also change app-properties

--- src/main/java/com/football_field/football_field/FootballFieldApplication.java
+++ src/main/java/com/football_field/football_field/FootballFieldApplication.java
@@ -2,8 +2,16 @@ package com.football_field.football_field;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 @SpringBootApplication
+//need to register JPA 2.1 converters so that all the Java 8 Date/Time
+//fields in the domain models automatically get converted to SQL types when we persist them in the database.
+@EntityScan(basePackageClasses = {
+		FootballFieldApplication.class,
+		Jsr310JpaConverters.class
+})
 public class FootballFieldApplication {
 
 	public static void main(String[] args) {
